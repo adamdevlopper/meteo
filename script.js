e4e4ca475ad70a3a7333729029704a23f15b1fa2
adamdevlopper <adamdevlopper@gmail.com>
2024-09-17 12:54:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+// Define the city to get the weather for
+// default city : Bruxelles (my city)
 let ville = "Bruxelles";
 
-
-
+// Fetch the temperature for the defined city
 recevoirTemperature(ville);
 
+// Add an event listener to the change city button
 let changerVilleButton = document.querySelector("#changer");
 
 changerVilleButton.addEventListener("click", () => {
+  // Prompt user to enter a new city
   choixVille = prompt("Ecrivez le ville de votre choix");
   recevoirTemperature(choixVille);
 });
 
-
+// Function to fetch the temperature for a given city
 function recevoirTemperature(ville) {
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
